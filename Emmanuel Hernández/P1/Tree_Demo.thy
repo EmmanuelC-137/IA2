9de7de2b75e6bea90429996de6a9c2dfac5ece4c
EmmanuelC-137 <179169@upslp.edu.mx>
2026-02-04 16:44:41 -0600

    fun mirror

--- Emmanuel Hernández/P1/Tree_Demo.thy
+++ Emmanuel Hernández/P1/Tree_Demo.thy
@@ -9,4 +9,8 @@ datatype 'a tree = Leaf | Node "'a tree" 'a "'a tree"
 lemma "Leaf \<noteq> Node l x r"
   by auto
 
+fun mirror :: "'a tree \<Rightarrow> 'a tree" where
+"mirror Leaf = Leaf" |
+"mirror (Node l x r) = Node (mirror r) x (mirror l)"
+
 end
