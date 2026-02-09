54622e9fc33ba9016331d25f99314931d5af6f9a
EmmanuelC-137 <179169@upslp.edu.mx>
2026-02-09 16:34:54 -0600

    Three_demo.thy complete

--- Emmanuel Hernández/P1/Tree_Demo.thy
+++ Emmanuel Hernández/P1/Tree_Demo.thy
@@ -13,4 +13,24 @@ fun mirror :: "'a tree \<Rightarrow> 'a tree" where
 "mirror Leaf = Leaf" |
 "mirror (Node l x r) = Node (mirror r) x (mirror l)"
 
+value "mirror (Node (Node Leaf a Leaf) b t)"
+
+thm tree.induct
+
+lemma mirror_mirror[simp]: "mirror (mirror t) = t"
+  apply (induction t)
+   apply auto
+  done
+
+thm mirror_mirror
+
+(* text <Ejemplo de una funcion que tiene más ecuaciones que contructores> *)
+
+fun sep :: "'a => 'a list => 'a list" where
+"sep x [] = []" |
+"sep x [a] = [a]" |
+"sep x (a # bs) = a # x # sep x bs"
+
+value "sep x [1,2,3,4,5::nat]"
+
 end
