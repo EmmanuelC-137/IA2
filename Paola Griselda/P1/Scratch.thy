a7d7e2d5305a4809ef14e62f3cbc26eaff4c3094
paolanavarror <181419@upslp.edu.mx>
2026-02-24 16:53:09 -0600

    s5

--- Paola Griselda/P1/Scratch.thy
+++ Paola Griselda/P1/Scratch.thy
@@ -1,32 +1,24 @@
-theory Scratch
-  imports Main
-begin 
+theory 1_AExp
+  imports
+begin
 
-datatype N = cero | suc N  (*se definene los dos constructores *)
+subsection \<open>\<close>
 
-(*definir recursivamente sobre el primer argumento*)
-fun suma :: "N \<Rightarrow> N \<Rightarrow> N" where
-"suma  cero y = y" |
-"suma (suc x) y = suc (suma x y)"
+type_synonym vname = string
+type_synonym val = int
+type_synonym state = "vname \<Rightarrow> val"
 
-value "suma (suc (suc cero)) (suc (suc cero))"
+(*Los strings en Isabelle se denotan por dos comillas simples*)
 
-fun mult :: "N \<Rightarrow> N \<Rightarrow> N" where
-"mult cero y = cero" |
-"mult (suc x) y = suma y (mult x y)"
+(*Defiición de la esructura de datos con la que vamos a representar las estructuras numericas*)
+(*Sintacticamente los entyero son diferentes de las cadenas y de las expresiones*)
+datatype aexp = N int | V vname | Plus aexp aexp
+thm aexp.distinct
 
-(*Herramientas para encontrar contra ejemplos quickchech y nitpick, si hay contrajemplos
-ya no hay necedidad de realizar la demostración ya que no va a ser posible*)
-
-lemma "mult (suma x y) z = suma (mult x z)"
-proof -
-  have assoc: "\<And>x y z. suma x (suma y z) = suma (suma x y) z"
-    by (induct_tac x, auto) (*puede apicar en las variables cuantificada, variables verdess*)
-  show ?thesis 
-  apply (induction x, auto simp add: assoc) (*no es suficiente, es decir, que se necesit aun lema para 
-    probar la conjetura, induction sobre variables azules *)
-  qed
+(*Recibe un estado, función *)
+fun aval :: "aexp \<Rightarrow> state \<Rightarrow> val" where
+"aval (N n) s = n" |
+"aval (V x) s = s x" |
+"aval (Plus a b) s = aval a s + aval b s "
 
 end
-
-
