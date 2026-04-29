609947db8a1409ca0f842fc0ff7f5585747fa530
paolanavarror <181419@upslp.edu.mx>
2026-04-28 23:29:23 -0600

    P3 - Corrección de lemmas con Isar

--- Paola Griselda/P3/Isar_Demo.thy
+++ Paola Griselda/P3/Isar_Demo.thy
@@ -1,46 +1,61 @@
 theory Isar_Demo
-imports Complex_Main
+  imports Complex_Main
 begin
 
 thm surj_def
 
-lemma "\<not> surj(f:: 'a \<Rightarrow> 'a set)"
-proof 
+lemma "\<not> surj(f::'a \<Rightarrow> 'a set)"
+proof
   assume 0: "surj f"
-  from 0 have 1: "\<forall>A. \<exists>A. A = f a"
+  from 0 have 1: "\<forall>A. \<exists>a. A = f a"
     by (simp add: surj_def)
   from 1 have 2: "\<exists>a. {x. x \<in> f x} = f a" 
     by blast
+  from 2 show "False"
+    by blast
+qed
   
-
-lemma "\<not> surj(f:: 'a \<Rightarrow> 'a set)"
+lemma "\<not> surj(f::'a \<Rightarrow> 'a set)"
 proof  (*de 0 brincamos a 2 sin pasar por 1*)
   assume 0: "surj f"
-  from 0 have 1: "\<exists>a. {x. x "(*Se pueden poner etiquetas para cada submeta *)
+  from 0 have 1: "\<exists>a. {x. x \<notin> f x} = f a"(*Se pueden poner etiquetas para cada submeta *)
+    by (auto simp: surj_def)
+  from 1 show "False" 
+    by blast
+qed
 
-
-    text \<open>\<close>
+text \<open>No es  necesario el uso de etiquetas\<close>
 (*La utima formula que se referenció con esa*)
 lemma "\<not>surj(f::'a \<Rightarrow> 'a set)"
 proof 
   assume "surj f"
-  then
+  from this have "\<exists>a. {x. x \<notin> f x} = f a"
+    by (auto simp: surj_def)
+  from this show "False"
+    by blast
+qed
 
-  text\<open>"then" = "from this">
+text\<open>"then" = "from this"\<close>
 (**)
 lemma "\<not>surj(f::'a \<Rightarrow> 'a set)"
 proof
   assume "surj f"
   then have "\<exists>a. {x. x \<notin> f x} = f a"
-  by (auto simp: surj_def)
-
+    by (auto simp: surj_def)
+  then show "False" 
+    by blast
+qed
 
 
 text\<open>"hence" = "then have", "thus" = "then show"\<close>
 lemma "\<not>surj(f::'a \<Rightarrow> 'a set)"
 proof
   assume "surj f"
-  hence "\<exists>a. "
+  hence "\<exists>a. {x. x \<notin> f x} = f a"
+    by (auto simp: surj_def)
+  thus "False"
+    by blast
+qed
 
 text \<open>Enunciados etructurados: "fixes", "assumes", "shows"\<close>
 lemma (*Demosrtración alternativa*)
@@ -50,7 +65,8 @@ lemma (*Demosrtración alternativa*)
 proof - 
   have "\<exists> a. {x. x \<notin> f x} = f a" using s
     by (auto simp: surj_def)
-  thus "False" by blast
+  thus "False" 
+    by blast
 qed
 
 section \<open>Patrones de prueba\<close>
@@ -72,11 +88,19 @@ next
 qed
 
 lemma "A \<subseteq> B" (*Probar algun conjunto de A es un subconjunto de B*)
-(*fijar un elemento a esta dentro del conjunto A, asumiendo *)
+proof 
+  fix a(*fijar un elemento a esta dentro del conjunto A, asumiendo *)
+  assume "a \<in> A"
+  show "a \<in> B" sorry
+qed
+
 
 text \<open>Contradicción\<close>
 lemma "P"
 proof (rule ccontr)
-  assume "
+  assume " \<not>P"
+  show "False" sorry
+qed
+
 end
 
