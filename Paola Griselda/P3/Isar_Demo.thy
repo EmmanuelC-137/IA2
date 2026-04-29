5653e6fac9b4e422368439b9a41279d2090e993a
paolanavarror <181419@upslp.edu.mx>
2026-04-29 16:52:11 -0600

    P3 - iSAR demosrración tarea

--- Paola Griselda/P3/Isar_Demo.thy
+++ Paola Griselda/P3/Isar_Demo.thy
@@ -9,11 +9,12 @@ proof
   assume 0: "surj f"
   from 0 have 1: "\<forall>A. \<exists>a. A = f a"
     by (simp add: surj_def)
-  from 1 have 2: "\<exists>a. {x. x \<in> f x} = f a" 
+  from 1 have 2: "\<exists>a. {x. x \<notin> f x} = f a"
     by blast
   from 2 show "False"
     by blast
 qed
+
   
 lemma "\<not> surj(f::'a \<Rightarrow> 'a set)"
 proof  (*de 0 brincamos a 2 sin pasar por 1*)
@@ -58,6 +59,7 @@ proof
 qed
 
 text \<open>Enunciados etructurados: "fixes", "assumes", "shows"\<close>
+
 lemma (*Demosrtración alternativa*)
   fixes f :: "'a \<Rightarrow> 'a set"
   assumes s: "surj f"
@@ -96,11 +98,68 @@ qed
 
 
 text \<open>Contradicción\<close>
+
+thm ccontr
+
 lemma "P"
 proof (rule ccontr)
   assume " \<not>P"
   show "False" sorry
 qed
+
+text \<open>Distinción de casos\<close>
+
+thm disjE (*regla de la eliminación de la disyunción*)
+
+lemma "R" (*Se quiere demostrar R*)
+proof cases (*Se  necesita cases para demostrar por casos*)
+  assume "P" (*asummos P y bajo esa suposición*)
+  show "R" sorry (*demostramos R*)
+next
+  assume "\<not>P"
+  show "R" sorry
+qed
+
+lemma "R"
+proof - (*colcamos - para que no se apique ninguna regla de introducción*)
+  have "P \<or> Q" sorry
+  then show "R"
+  proof 
+    assume "P"
+    show "R" sorry
+  next
+    assume "Q"
+    show "R" sorry
+  qed
+qed
+
+thm exI (*regla de la introducción de la exixtencial*)
+(* el x es una metavariable entonces puedeser sustituida por lo que se quiera *)
+
+lemma "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume "surj f"
+  hence "\<exists>a. {x. x \<notin> f x} = f a"
+    by (auto simp: surj_def)
+  then obtain a where "{x. x \<notin> f x} = f a" (*Todo el ejemplo circula en usar obtain y quietar el cuantificador exitencial *)
+    by blast
+  hence "a \<notin> f a \<longleftrightarrow> a \<in> f a"
+    by blast
+  thus "False" 
+    by blast
+qed
+
+lemma
+  assumes "\<exists>x. \<forall>y. P x y"
+    shows "\<forall>y. \<exists>x. P x y"
+      proof
+        fix y
+        
+
+
+
+
+
 
 end
 
