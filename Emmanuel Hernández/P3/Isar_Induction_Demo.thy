fd42f315e3e244eb7584a8ec12f43228720be496
EmmanuelC-137 <179169@upslp.edu.mx>
2026-05-18 16:46:10 -0600

    Mas conceptos en Isar induction

--- Emmanuel Hernández/P3/Isar_Induction_Demo.thy
+++ Emmanuel Hernández/P3/Isar_Induction_Demo.thy
@@ -6,7 +6,7 @@ section "Case distinction and induction"
 
 subsection "Case distinction"
 
-text ‹Explicit: ›
+text \<open>Explicit: \<close>
 
 lemma "length(tl xs) = length xs - 1"
 proof (cases xs)
@@ -16,7 +16,7 @@ next
   thus ?thesis by simp
 qed
 
-text ‹Implicit: ›
+text \<open>Implicit: \<close>
 
 lemma "length(tl xs) = length xs - 1"
 proof (cases xs)
@@ -30,10 +30,10 @@ next
   thus ?thesis by simp
 qed
 
-subsubsection ‹Structural induction for type @{typ nat}›
+subsubsection \<open>Structural induction for type @{typ nat}\<close>
 
-text ‹Explicit: ›
-lemma "∑{0..n::nat} = n*(n+1) div 2" (is "?P n")
+text \<open>Explicit: \<close>
+lemma "\<Sum>{0..n::nat} = n*(n+1) div 2" (is "?P n")
 proof (induction n)
   show "?P 0" by simp
 next
@@ -41,55 +41,5 @@ next
   thus "?P(Suc n)" by simp
 qed
 
-text  ‹In more details: ›
-
-lemma "∑{0..n::nat} = n*(n+1) div 2" (is "?P n")
-proof (induction n)
-  show "?P 0" by simp
-next
-  fix n assume IH: "?P n"
-  have "∑{0..Suc n} = ∑{0..n} + Suc n" by simp
-  also have "... = n*(n+1) div 2 + Suc n" using IH by simp
-  also have "... = (Suc n) * ((Suc n)+1) div 2" by simp
-  finally show "?P(Suc n)" .
-qed
-
-text  ‹Implicit: ›
-
-lemma "∑{0..n::nat} = n*(n+1) div 2" (is "?P n")
-proof (induction n)
-  print_cases
-  case 0
-  show ?case by simp
-next
-  case (Suc n)
-  thm Suc
-  thus ?case by simp
-qed
-
-
-text  ‹Induction with ‹\<Longrightarrow>›: ›
-
-lemma split_list: "x: set xs \<Longrightarrow> \<exists>ys zs. xs = ys @ x # zs"
-proof (induction xs)
-  case Nil thus ?case by simp
-next
-  case (Cons a xs)
-  thm Cons.IH (*Introduccion de hipotesis*)
-  thm Cons.prems (*Premises of the step case*)
-  thm Cons
-  from Cons.prems have "x = a \<or> x: set xs" by simp
-  thus ?case
-  proof
-    assume "x = a"
-    hence "a#xs = [] @ x # xs" by simp
-    thus ?thesis by blast
-  next
-    assume "x: set xs"
-    then obtain ys zs where "xs = ys @ x # zs" using Cons.IH by auto
-      hence "a#xs = (a#ys) @ x # zs" by simp
-      thus ?thesis by blast
-    qed
-qed
 
 end
